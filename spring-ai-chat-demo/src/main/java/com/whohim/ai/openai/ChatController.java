028f2fdf4958b138475d9969b383e6836ebd0f73
white cat <whoqian@gmail.com>
2025-03-23 12:26:29 +0800

    add 上下文对话 demo

--- spring-ai-chat-demo/src/main/java/com/whohim/ai/openai/ChatController.java
+++ spring-ai-chat-demo/src/main/java/com/whohim/ai/openai/ChatController.java
@@ -1,10 +1,21 @@
 package com.whohim.ai.openai;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.InMemoryChatMemory;
+import org.springframework.ai.chat.messages.AssistantMessage;
+import org.springframework.ai.chat.messages.Message;
+import org.springframework.ai.chat.messages.UserMessage;
 import org.springframework.http.MediaType;
+import org.springframework.http.codec.ServerSentEvent;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class ChatController {
@@ -15,9 +26,10 @@ public class ChatController {
         this.chatClient = chatClient.build();
     }
 
+    private final ChatMemory chatMemory = new InMemoryChatMemory();
+
     @GetMapping("/demo")
     public String chat(String userInput) {
-
         return this.chatClient.prompt()
                 .user(userInput)
                 .call()
@@ -25,12 +37,61 @@ public class ChatController {
     }
 
     // 流式调用 将produces声明为文本事件流
-    @GetMapping(value = "/stream",produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-    public Flux<String> stream(String prompt){
+    @GetMapping(value = "/stream", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<String> stream(String prompt) {
         Flux<String> output = chatClient.prompt()
                 .user(prompt)
                 .stream()
                 .content();
         return output;
     }
+
+    /**
+     * 根据会话id，从数据库中查找历史消息，并将消息作为上下文回答。
+     *
+     * @param prompt    用户的提问
+     * @param sessionId 会话id
+     * @return SSE流响应
+     */
+    @GetMapping(value = "chat/stream/history", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<ServerSentEvent<String>> chatStreamWithHistory(@RequestParam String prompt,
+                                                               @RequestParam String sessionId) {
+        // 1. 如果需要存储会话和消息到数据库，自己可以实现ChatMemory接口，
+        //    这里使用InMemoryChatMemory，内存存储。
+        // 2. 传入会话id，MessageChatMemoryAdvisor会根据会话id去查找消息。
+        // 3. 只需要携带最近10条消息
+        var messageChatMemoryAdvisor = new MessageChatMemoryAdvisor(chatMemory, sessionId, 10);
+        return chatClient.prompt()
+                .user(prompt)
+                // MessageChatMemoryAdvisor会在消息发送给大模型之前，从ChatMemory中获取会话的历史消息，
+                // 然后一起发送给大模型。
+                .advisors(messageChatMemoryAdvisor)
+                .stream()
+                .content()
+                .map(chatResponse -> ServerSentEvent.builder(chatResponse)
+                        .event("message")
+                        .build());
+    }
+
+
+//    上下文对话 demo
+    static List<Message> historyMessage = new ArrayList<>();
+    static int maxLen = 10;
+
+    @GetMapping("/context")
+    public String context(String prompt) {
+        // 用户输入的文本是UserMessage
+        historyMessage.add(new UserMessage(prompt));
+        // 发给AI前对历史消息对列的长度进行检查
+        if (historyMessage.size() > maxLen) {
+            historyMessage = historyMessage.subList(historyMessage.size() - maxLen - 1, historyMessage.size());
+        }
+        // 获取AssistantMessage
+        ChatClient.CallResponseSpec call = chatClient.prompt().messages(historyMessage).call();
+        AssistantMessage assistantMessage = call.chatResponse().getResult().getOutput();
+        // 将AI回复的消息放到历史消息列表中
+        historyMessage.add(assistantMessage);
+        return assistantMessage.getContent();
+    }
+
 }
